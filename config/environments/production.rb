59d1a828f5db4696a629b698cc7286ef7b2f6658
Konstantin K <konstantin.koss@gmail.com>
2014-01-23 20:29:06 +0700

    disable email errors on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ EmployeeReporter::Application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.default_url_options = { :host => 'http://employeereport.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = false
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
